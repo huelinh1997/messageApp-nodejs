7a61bda2f953ae18faea3681c06dc9c4e6459c37
Linh Nguyen <linh.nguyen@miracles.vn>
2020-06-10 23:54:02 +0700

    change connect

--- api/db.js
+++ api/db.js
@@ -2,10 +2,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-	host: process.env.DB_HOST || "localhost",
-	user: process.env.DB_USER || "root",
-	password: process.env.DB_PASSWORD || "12345678",
-	database: process.env.DB_NAME || "message_notify",
+	host: process.env.DB_HOST || "mysql5006.site4now.net",
+	user: process.env.DB_USER || "9a9cca_infolue",
+	password: process.env.DB_PASSWORD || "Vbn*34295",
+	database: process.env.DB_NAME || "db_9a9cca_infolue",
 });
 
 module.exports = db;
